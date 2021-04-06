4853a2340ccc9f66fc7344f2d8233bc225fdf44b
Cornel <cornel@borina.ro>
2021-04-06 09:08:07 +0300

    build: github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  router: {
+    base: "/nuxt-playground/",
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "nuxt-playground",
